078f074f902d4f09f100b516f0467a5c93454a8f
akiyumikiku <nguyenbinhminh77211@gmail.com>
2025-10-03 00:39:55 +0700

    Update messageDeleteBot.js

--- events/messageDeleteBot.js
+++ events/messageDeleteBot.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
   client.on("messageCreate", async (message) => {
     try {
-      // Chỉ xử lý tin nhắn từ bot có ID kia
+      // ID bot cần xoá tin nhắn
       if (message.author.id !== "1335725304697720922") return;
 
       // Nếu chứa cụm text cần xóa
