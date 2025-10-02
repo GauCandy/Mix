d46b7411e5d7779fe6fe6978a7938d246ea0c943
akiyumikiku <nguyenbinhminh77211@gmail.com>
2025-10-03 00:20:23 +0700

    Update interaction.js

--- events/interaction.js
+++ events/interaction.js
@@ -1,18 +1,19 @@
-module.exports = {
-  name: "interactionCreate",
-  async execute(interaction, client) {
-    if (!interaction.isChatInputCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
+const { EmbedBuilder } = require("discord.js");
 
-    try {
-      await command.execute(interaction, client);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: "❌ Lỗi khi chạy lệnh này.",
-        ephemeral: true,
-      });
-    }
-  },
+module.exports = (client, rules) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isStringSelectMenu()) return;
+    if (interaction.customId !== "rules_menu") return;
+
+    const data = rules[interaction.values[0]];
+    if (!data) return;
+
+    const embed = new EmbedBuilder()
+      .setTitle(data.title)
+      .setDescription(data.desc)
+      .setColor(data.color)
+      .setImage(data.image || null);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+  });
 };
