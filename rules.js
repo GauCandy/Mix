73cab743434e63fa50b61ba661e3bfea62a776d7
akiyumikiku <nguyenbinhminh77211@gmail.com>
2025-10-07 00:19:01 +0700

    Update rules.js

--- rules.js
+++ rules.js
@@ -6,7 +6,7 @@ module.exports = {
 *Excessive Begging*\nDescription: Repeatedly asking for favors, items, roles, or other benefits disruptively.\n*Exceptions:* Jokingly begging\n\n
 *XP Farming*\nDescription: Sending messages solely to gain XP (Arcane bot).\nPunishments:\n1st Offense = reminder\n2nd Offense = Reduce XP\n3rd Offense = Level Reset`,
     color: "#ffffcc",
-    image: "https://cdn.discordapp.com/attachments/1358092861303947485/1358860213138096199/IMG_2287.png"
+    image: "https://media.discordapp.net/attachments/1411987904980586576/1424797126034067586/standard_10.gif?ex=68e5413a&is=68e3efba&hm=1e7e6ea034fb156d1744afcca8a41aa67c3bfa260d2834ec98b55125d5c490f7&=&width=1214&height=70"
   },
   opt2: {
     title: " **Channel Misuses**",
@@ -16,7 +16,7 @@ module.exports = {
 **Community Channels Misuse**\nUsing community section for inappropriate purposes.\nPunishments similar to above.\n\n
 **Voice Channel Misuse**\nImproperly using voice channels (Additional 1h Mute)`,
     color: "#7fe390",
-    image: "https://cdn.discordapp.com/attachments/1358092861303947485/1359923248946483382/Channel_Misuse.png"
+    image: "https://media.discordapp.net/attachments/1411987904980586576/1424797127669710991/standard_7.gif?ex=68e5413a&is=68e3efba&hm=65e0a6e36b5505a2ae0bb80151c8d7adaaa14f28284698884929767a2d60d7d6&=&width=1214&height=70"
   },
   opt3: {
     title: " **2 Warning Rules**",
@@ -28,7 +28,7 @@ module.exports = {
 *Toxicity*\nDisruptive behavior without exceptions.\n\n
 *Advertising/Self Promotion*\nPromoting media for self-gain without permission.`,
     color: "#f0954b",
-    image: "https://cdn.discordapp.com/attachments/1358092861303947485/1358860209686057240/IMG_2288.png"
+    image: "https://media.discordapp.net/attachments/1411987904980586576/1424797127132975185/standard_8.gif?ex=68e5413a&is=68e3efba&hm=221f73c9be91bc85ffe0ae191f4a7989a1ab72d8a18abf4c3240efd1b296e937&=&width=1214&height=70"
   },
   opt4: {
     title: " **3 Warning Rules**",
@@ -37,7 +37,7 @@ module.exports = {
 **DM Harassment**\nHarassing users via DMs from mutual server.\n\n
 **Discrimination**\nExtreme harmful stereotyping (severe = Hate Speech).`,
     color: "#f4363f",
-    image: "https://cdn.discordapp.com/attachments/1358092861303947485/1358860210349019246/IMG_2289.png"
+    image: "https://media.discordapp.net/attachments/1411987904980586576/1424797126462017546/standard_9.gif?ex=68e5413a&is=68e3efba&hm=7c9a400ef5cb3191766d2acda6c6a457e48aeb1a2f229b8f7192853dc7eb3874&=&width=1214&height=70"
   },
   opt5: {
     title: " **Instant Ban Rules**",
@@ -48,6 +48,6 @@ module.exports = {
 *Cybercrimes*\nIllegal activity online.\n\n
 *Inappropriate Profile*\nOffensive/suggestive profile. Punishment = Kick → Ban if unchanged.`,
     color: "#f13bfe",
-    image: "https://cdn.discordapp.com/attachments/1358092861303947485/1358860210986287224/IMG_2290.png"
+    image: "https://media.discordapp.net/attachments/1411987904980586576/1424797125581213886/standard_11.gif?ex=68e5413a&is=68e3efba&hm=6a6d7f9fe9c882285eb89816c4fc0fbcdbd22c0060e42bbc2ea2a758c3eea547&=&width=1214&height=70"
   }
 };
