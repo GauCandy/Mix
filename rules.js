d4b7cbf0b81e9bdd6fb43a72b18c0409f98cd040
akiyumikiku <nguyenbinhminh77211@gmail.com>
2025-10-07 15:38:54 +0700

    Update rules.js

--- rules.js
+++ rules.js
@@ -11,37 +11,38 @@ module.exports = {
   opt2: {
     title: " **Channel Misuses**",
     desc: `Channel misuses fall under the 1 Warning Rules category, meaning any kind of misuse will result in 1 warning.\nAnd possibly a blacklist.\n\n
-*Chatting Channel Misuse*\nDescription: Using the chatting channels for purposes other than chatting\nIncludes: Bot commands outside of the Bots channel.\n\n
-*Feature Request Misuse*\nDescription: Using feature request channels without following the intended guidelines\n1x: Warn + 1 Day FR Channel Blacklist\n2x: Warn + 1 Week FR Channel Blacklist\n3x: Warn + Perm FR Channel Blacklist\nGuidelines stated in the Rule Document and channel.\n\n
-*Voice Channel Misuse*\nDescription: Misusing / improperly utilizing the voice channels\n(Additional 1h Mute)`,
+**Chatting Channel Misuse**\nDescription: Using the chatting channels for purposes other than chatting\nIncludes: Bot commands outside of the Bots channel.\n\n
+**Webhook Channel Misuse**\nDesctription: Using the channel for the wrong purpose\n*1x: Warn + 1 Day Webhook Channel Blacklist*\n*2x: Warn + 1 Week Webhook Channel Blacklist*\n*3x: Warn + Perm Webhook Channel Blacklist*\n\n
+**Macro Request Channel Misuse**\nDescription: Using macro request channels without following the intended guidelines\n1x: Warn + 1 Day MR Channel Blacklist\n2x: Warn + 1 Week MR Channel Blacklist\n3x: Warn + Perm MR Channel Blacklist.\nGuidelineshttps://discord.com/channels/1410980858583715970/1411034286429306940/1411037311625330810⁠\n\n
+**Voice Channel Misuse**\nDescription: Misusing / improperly utilizing the voice channels\n(Additional 1h Mute)`,
     color: "#7fe390",
     image: "https://media.discordapp.net/attachments/1411987904980586576/1424797127669710991/standard_7.gif"
   },
   opt3: {
     title: " **2 Warning Rules**",
-    desc: `*Inappropriate/Suggestive Language*\nDescriptions: Implying/Referencing something inappropriate, offensive, or sexual.\nDepending on the severity, this can go up to 3 Warnings or fall into NSFW.\n\n
-*Toxicity*\nDescription: Engaging in disruptive behavior that can be seen as disrespectful or toxic in any way.\n*Exceptions:* Harmless arguments, Disagreement about Sol’s RNG related topic, discussions, constructive criticism, debates, joking with consent\n\n
-*Advertising/Self Promotion*\nDescription: Promoting/Spreading one's media for self gain.\n*Exceptions:* With permission`,
+    desc: `**Inappropriate/Suggestive Language**\nDescriptions: Implying/Referencing something inappropriate, offensive, or sexual.\nDepending on the severity, this can go up to 3 Warnings or fall into NSFW.\n\n
+**Toxicity**\nDescription: Engaging in disruptive behavior that can be seen as disrespectful or toxic in any way.\n*Exceptions:* Harmless arguments, Disagreement about Sol’s RNG related topic, discussions, constructive criticism, debates, joking with consent\n\n
+**Advertising/Self Promotion**\nDescription: Promoting/Spreading one's media for self gain.\n*Exceptions:* With permission`,
     color: "#f0954b",
     image: "https://media.discordapp.net/attachments/1411987904980586576/1424797127132975185/standard_8.gif?ex=68e5413a&is=68e3efba&hm=221f73c9be91bc85ffe0ae191f4a7989a1ab72d8a18abf4c3240efd1b296e937&=&width=1214&height=70"
   },
   opt4: {
     title: " **3 Warning Rules**",
-    desc: `*Mod Bait*\nDescription: Sending messages that appear punishable to provoke a reaction, even though they do not violate rules.\n\n
-*Accusation w/o Evidence*\nDescription: Making statements or claims about an individual wrongfully and without evidence to put them in a compromised situation\nDepending on the severity, this can lead to a ban.\n\n
-*Discrimination*\nDescription: Harmful stereotyping based on someone’s race, gender, sexuality, religion, or any other personal characteristics.\nDepending on the severity of the discrimination, extreme/severe cases can fall under “Hate Speech.”`,
+    desc: `**Mod Bait**\nDescription: Sending messages that appear punishable to provoke a reaction, even though they do not violate rules.\n\n
+**Accusation w/o Evidence**\nDescription: Making statements or claims about an individual wrongfully and without evidence to put them in a compromised situation\nDepending on the severity, this can lead to a ban.\n\n
+**Discrimination**\nDescription: Harmful stereotyping based on someone’s race, gender, sexuality, religion, or any other personal characteristics.\nDepending on the severity of the discrimination, extreme/severe cases can fall under “Hate Speech.”`,
     color: "#f4363f",
     image: "https://media.discordapp.net/attachments/1411987904980586576/1424797126462017546/standard_9.gif?ex=68e5413a&is=68e3efba&hm=7c9a400ef5cb3191766d2acda6c6a457e48aeb1a2f229b8f7192853dc7eb3874&=&width=1318&height=76"
   },
   opt5: {
     title: " **Instant Ban Rules**",
-    desc: `*Roblox & Discord ToS*\nDescription: This server will enforce the Terms of Service of both platforms, failure to follow them will result in a ban\nhttps://discord.com/guidelines\nhttps://discord.com/guidelines\nhttps://en.help.roblox.com/hc/en-us/articles/115004647846-Roblox-Terms-of-Use\n\n
-*Punishment Evading*\nDescription: Leaving to avoid any punishment of any kind.\n\n
-*NSFW*\nDescription: Content that is inappropriate for any professional or public place, usually including nudity or suggestiveness of any kind.\n\n
-*Hate Speech/Racism*\nDescription: Any form of imagery / discussions that promotes extreme discrimination and hatred against individuals or groups based on their characteristics\n\n
-*Child Endangerment*\nDescription: Any act that puts minors at harm or inappropriate exposure.\n\n
-*Cybercrimes*\nDescription: Any form of illegal activity online\n\n
-*Inappropriate Profile*\nDescription: Using an inappropriate Discord profile picture, Discord display name, or Roblox username that contains offensive, suggestive, or explicit content.\nPunishments: Kick. If rejoined and unchanged, Ban`,
+    desc: `*Roblox & Discord ToS*\nDescription: This server will enforce the Terms of Service of both platforms, failure to follow them will result in a ban\n[Discord Community Guidelines](https://discord.com/guidelines)\n[Discord Tos](https://discord.com/guidelines)\n[Roblox Tos](https://en.help.roblox.com/hc/en-us/articles/115004647846-Roblox-Terms-of-Use)",
+**Punishment Evading**\nDescription: Leaving to avoid any punishment of any kind.\n\n
+**NSFW**\nDescription: Content that is inappropriate for any professional or public place, usually including nudity or suggestiveness of any kind.\n\n
+**Hate Speech/Racism**\nDescription: Any form of imagery / discussions that promotes extreme discrimination and hatred against individuals or groups based on their characteristics\n\n
+**Child Endangerment**\nDescription: Any act that puts minors at harm or inappropriate exposure.\n\n
+**Cybercrimes**\nDescription: Any form of illegal activity online\n\n
+**Inappropriate Profile**\nDescription: Using an inappropriate Discord profile picture, Discord display name, or Roblox username that contains offensive, suggestive, or explicit content.\nPunishments: Kick. If rejoined and unchanged, Ban`,
     color: "#f13bfe",
     image: "https://media.discordapp.net/attachments/1411987904980586576/1424797125581213886/standard_11.gif?ex=68e5413a&is=68e3efba&hm=6a6d7f9fe9c882285eb89816c4fc0fbcdbd22c0060e42bbc2ea2a758c3eea547&=&width=1214&height=70"
   }
